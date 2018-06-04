30022a9a19d0402f7e08755e6e2a76d610f2c848
BeryNice <berenice.lemus@gmail.com>
2018-06-04 01:02:31 -0500

    subiendo archivos ejercicio convertidor de temperatura

---
+++ app.js
@@ -0,0 +1,7 @@
+// Pregubtar temperatura en celcius
+var temperature = parseInt(prompt("¿Cuál es la temperatura en Celcius?"));
+// convertir celcius a Farenheit
+var convertToFarenheith = (temperature*1.8) + 32;
+//Mostrar resultado
+console.log( "La temperatura en Farenheith es " + convertToFarenheith);
+document.write( "La temperatura en Farenheith es " + convertToFarenheith);
